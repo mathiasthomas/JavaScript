981665e22e781ff2c0fa094c2957f0bb4513ff95
thomas mutavi <mathiasthomas17@gmail.com>
2022-04-29 16:37:23 +0300

    commit if else statements

---
+++ conditional.js
@@ -0,0 +1,9 @@
+const year = 2022;
+const myAge = 15;
+const elligible = year - myAge;
+
+if (elligible >= 18) {
+    console.log('Congaratulations You are elligible for a drivers license');
+} else {
+    console.log('Sorry you are not elligible for a drivers lisence');
+}
