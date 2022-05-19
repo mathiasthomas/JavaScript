85a4658b71dd2bfd7bc87265c34f51a9663ba2ee
thomas mutavi <mathiasthomas17@gmail.com>
2022-05-19 10:44:39 +0300

    commit

--- part 2/05-Guess-My-Number/starter/script.js
+++ part 2/05-Guess-My-Number/starter/script.js
@@ -1,2 +1,40 @@
 'use strict';
-console.log(document.querySelector('.message').textContent);
+// domument object models ()
+// console.log(document.querySelector('.message').textContent);
+/*
+ document.querySelector('.guess').value;
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 170;
+document.querySelector('.guess').value = 23;
+*/
+
+//events handling
+const number = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = number;
+let score = 20;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const quesss = Number(document.querySelector('.guess').value);
+  console.log(quesss);
+  if (!quesss) {
+    document.querySelector('.message').textContent = '🚨 Please Insert Number';
+  } else if (quesss === number) {
+    document.querySelector('.message').textContent =
+      '💕 Congrats, Corect Number!!!!';
+  } else if (quesss > number) {
+    if (score > 1) {
+      document.querySelector('.message').textContent =
+        '⛔  Sorry Quess Is Too High';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent =
+        '💔💔  Failed!!! You Lost The Game';
+    }
+  } else if (quesss < number) {
+    document.querySelector('.message').textContent =
+      ' ❌ Sorry Quess Is Too Low';
+    score--;
+    document.querySelector('.score').textContent = score;
+  }
+});
