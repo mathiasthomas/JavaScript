bd8dc3226e3a11f892eef8563d3c16a3f3c99f6b
thomas mutavi <mathiasthomas17@gmail.com>
2022-06-02 04:28:11 +0300

    commit

--- part 2/06-Modal/starter/script.js
+++ part 2/06-Modal/starter/script.js
@@ -1,13 +1,44 @@
 'use strict';
 const model = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
-const btnClose = document.querySelector('.close-model');
+const btnClose = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 // console.log(btnsOpenModal);
+//openModal Function
+const openModal = function () {
+  console.log('button clicked');
+  model.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
 
 for (let i = 0; i < btnsOpenModal.length; i++) {
-  btnsOpenModal[i].addEventListener('click', function () {
-    console.log('button clicked');
-    model.classList.remove('hidden');
-  });
+  btnsOpenModal[i].addEventListener('click', openModal);
 }
+//closeModal Function
+const closeModal = function () {
+  model.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+btnClose.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+// btnClose.addEventListener('click', function () {
+//   model.classList.add('hidden');
+//   overlay.classList.add('hidden');
+// });
+// overlay.addEventListener('click', function () {
+//   model.classList.add('hidden');
+//   overlay.classList.add('hidden');
+// });
+
+//Listening to keyboard events
+//there are three keyboard keys
+//  keydown,keyup and key press
+document.addEventListener('keydown', function (event) {
+  if (event.key === 'Escape') {
+    if (!model.classList.contains('hidden')) {
+      closeModal();
+    }
+  }
+  xc;
+});
